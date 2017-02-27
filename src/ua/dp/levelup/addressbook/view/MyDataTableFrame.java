4f9e7c5040595d3dc412c74e1a906965c6b9a9d7
andreydp <poletaiev@gmail.com>
2017-02-27 22:08:16 +0200

    A lot of changes. Add DAO part

---
+++ src/ua/dp/levelup/addressbook/view/MyDataTableFrame.java
@@ -0,0 +1,35 @@
+package ua.dp.levelup.addressbook.view;
+
+import ua.dp.levelup.addressbook.view.impl.CitizenTablePanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by andrey on 12.01.2017.
+ */
+public class MyDataTableFrame extends JFrame
+{
+
+    public MyDataTableFrame()
+    {
+        init();
+    }
+
+    private void init()
+    {
+        Container container = getContentPane();
+
+        TabbedPane tabbedPane = new TabbedPane();
+
+        CitizenTablePanel citizenTablePanel = new CitizenTablePanel();
+        tabbedPane.add(citizenTablePanel);
+
+        container.add(tabbedPane, BorderLayout.CENTER);
+        container.add(new ToolPanel(tabbedPane), BorderLayout.PAGE_END);
+
+        setBounds(0, 0, 800, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
